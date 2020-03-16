2ca7af11482fc7587e38dbd4e7a646d79626d303
Caitlin-B <56935291+Caitlin-B@users.noreply.github.com>
2020-03-16 15:40:10 +0000

    added api req to get route city

--- src/api.js
+++ src/api.js
@@ -31,3 +31,8 @@ export const postLogIn = (username, password) => {
   });
 };
 
+
+export const getRouteCity = (coordinates) => {
+  return axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${coordinates[0]},${coordinates[1]}.json?access_token=pk.eyJ1IjoiY2FpdGxpbi1iIiwiYSI6ImNrN2cwNGxqMzA3cTYzZW1wdGNmN3lrNHMifQ.cAnpnGVhEh0RQTtHeYDxUg
+  `).then(res =>  res.data.features[3].text)
+}
