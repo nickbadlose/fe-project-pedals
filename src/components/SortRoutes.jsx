f64a882312cd6103d160100f5d4440a516a94593
Anna Lydon <annalydon@annas-mbp.mynet>
2020-03-18 14:57:18 +0000

    sort and search button styling added

--- src/components/SortRoutes.jsx
+++ src/components/SortRoutes.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import DropdownButton from "react-bootstrap/DropdownButton";
+// import Dropdown from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
 import styles from "./styling/SortRoutes.module.css";
 
@@ -10,30 +10,49 @@ class SortRoutes extends Component {
   };
   render() {
     return (
-      <DropdownButton
-        id="dropdown-basic-button"
-        title="Sort by"
-        className={styles.sortButton}
-        onSelect={this.onSelect}
-      >
-        <Dropdown.Item eventKey="posted/desc">Newest</Dropdown.Item>
-        <Dropdown.Item eventKey="posted/asc">Oldest</Dropdown.Item>
-        <Dropdown.Item eventKey="calculatedDistance/desc">
-          Distance: Longest to Shortest
-        </Dropdown.Item>
-        <Dropdown.Item eventKey="calculatedDistance/asc">
-          Distance: Shortest to Longest
-        </Dropdown.Item>
-        <Dropdown.Item eventKey="averageRating/desc">
-          Rating: Highest to Lowest
-        </Dropdown.Item>
-        <Dropdown.Item eventKey="averageRating/asc">
-          Rating: Lowest to Highest
-        </Dropdown.Item>
-      </DropdownButton>
+      <Dropdown>
+        <Dropdown.Toggle
+          id="dropdown-basic-button"
+          className={styles.sortButton}
+          onSelect={this.onSelect}
+        >
+          Sort by
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item className={styles.dropdownItem} eventKey="posted/desc">
+            Newest
+          </Dropdown.Item>
+          <Dropdown.Item className={styles.dropdownItem} eventKey="posted/asc">
+            Oldest
+          </Dropdown.Item>
+          <Dropdown.Item
+            className={styles.dropdownItem}
+            eventKey="calculatedDistance/desc"
+          >
+            Distance: Longest to Shortest
+          </Dropdown.Item>
+          <Dropdown.Item
+            className={styles.dropdownItem}
+            eventKey="calculatedDistance/asc"
+          >
+            Distance: Shortest to Longest
+          </Dropdown.Item>
+          <Dropdown.Item
+            className={styles.dropdownItem}
+            eventKey="averageRating/desc"
+          >
+            Rating: Highest to Lowest
+          </Dropdown.Item>
+          <Dropdown.Item
+            className={styles.dropdownItem}
+            eventKey="averageRating/asc"
+          >
+            Rating: Lowest to Highest
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     );
   }
-
 
   onSelect = eventKey => {
     const separatedEventKey = eventKey.split("/");
