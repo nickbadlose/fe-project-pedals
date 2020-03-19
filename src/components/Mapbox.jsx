28826968f0a1ca1cc3fcb64313e24a2c9f1062b4
Caitlin-B <56935291+Caitlin-B@users.noreply.github.com>
2020-03-19 09:42:39 +0000

    fixed popups

--- src/components/Mapbox.jsx
+++ src/components/Mapbox.jsx
@@ -12,8 +12,8 @@ import Button from "react-bootstrap/Button";
 import bike_spinner from "./icons/bike_spinner.gif";
 import * as api from "../api.js";
 import { navigate } from "@reach/router";
-import attractionFlag from "./icons/location-pin.png";
-import warningFlag from "./icons/warning-flag.png";
+import attractionFlag from "./icons/orange_marker.png";
+import warningFlag from "./icons/orange_flag.png";
 
 const Map = ReactMapboxGl({
   accessToken:
@@ -73,7 +73,9 @@ class Mapbox extends Component {
       onClickMap,
       handleMarkerForm,
       handleMarkerFormChange,
-      setMarkerType
+      setMarkerType,
+      setSelectedMarker,
+      closePopup
     } = this;
     return (
       <div className={styles.map_block}>
@@ -120,7 +122,7 @@ class Mapbox extends Component {
                       src={markerImage}
                       height="30px"
                       onClick={() => {
-                        // setSelectedMarker(feature);
+                        setSelectedMarker(feature);
                       }}
                     />
                   </Marker>
@@ -130,7 +132,7 @@ class Mapbox extends Component {
             {selectedMarker && (
               <Popup
                 coordinates={selectedMarker.geometry.coordinates}
-                // onClick={closePopup}
+                onClick={closePopup}
               >
                 <p>{selectedMarker.markerComments[0]}</p>
               </Popup>
@@ -662,6 +664,14 @@ class Mapbox extends Component {
           localStorage.setItem("routeDescription", routeDescription);
         });
     }
+  };
+
+  setSelectedMarker = feature => {
+    this.setState({ selectedMarker: feature });
+  };
+
+  closePopup = () => {
+    this.setState({ selectedMarker: null });
   };
 
   handleRouteTypeChange = e => {
